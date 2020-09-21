b5cab9da78d918961ddc3126f6d15e9f8cc99ef7
egrinberg <46496977+egrinberg@users.noreply.github.com>
2020-09-21 15:11:09 -0400

    0.1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Utest
-Command-line tool for finding and reporting dead links in a file
+
+Command-line tool for finding and reporting dead links in a file written in node.js
+
+**Features**
+
+The tool finds all URLs in the file by using the following schemes:
+-http://
+
+-https://
+
+The tool supports the following status codes:
+
+-200
+
+-400
+
+-404
+
+**Installation**
+
+\*Clone the repository to your local machine
+
+\*Install the tool globally by running the following command
+
+> npm i -g Utest
+
+**Usage**
+
+Find and check URLs inside file:
+
+> Utest -f file.txt
+
+Version information:
+
+> Utest -v
+
+Help:
+
+> Utest
---
+++ Utest.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+const yargs = require("yargs");
+const fetch = require("node-fetch");
+const v = require("./package.json").version;
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555",
+};
+
+const vmessage = chalk.white.bold(`Utest version: ${v}`);
+const msgBox = boxen(vmessage, boxenOptions);
+
+const argv = yargs
+  .usage("Usage: $0 [option]")
+  .alias("f", "file")
+  .nargs("f", 1)
+  .describe("f", "Load a file")
+  .demandOption(["f"])
+  .help("h")
+  .alias("h", "help")
+  .alias("v", "version")
+  .version(`${msgBox}`)
+  .describe("version", "show version information").argv;
+
+const fs = require("fs");
+
+// Create stream with the file
+const s = fs.createReadStream(argv.file);
+
+var urlList;
+s.on("data", (buf) => {
+  // Get all the URL links from the file
+  urlList = buf
+    .toString()
+    .match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi);
+});
+
+s.on("end", () => {
+  //Iterate through the links and check their status
+  urlList.forEach(async (url) => {
+    try {
+      const urlTest = await fetch(url, { method: "head" });
+
+      if (urlTest.status == 200) {
+        console.log(
+          chalk.green.bold(`[GOOD] Status: [${urlTest.status}] ${url}`)
+        );
+      } else if (urlTest.status == 400 || urlTest.status == 404) {
+        console.log(chalk.red.bold(`[BAD] Status: [${urlTest.status}] ${url}`));
+      } else {
+        console.log(
+          chalk.grey.bold(`[UNKNOWN] Status: [${urlTest.status}] ${url}`)
+        );
+      }
+    } catch (error) {
+      console.log(chalk.grey.bold(`[UNKNOWN] Status: [UNKNOWN] ${url}`));
+    }
+  });
+});
