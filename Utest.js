c6184dbdeb8669cce8a7364412ddbd6f8b2ee00f
Kate Grinberg <egrinberg@myseneca.ca>
2020-11-19 10:38:18 -0500

    Move file processing functions into a separate module

--- Utest.js
+++ Utest.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
-
 const chalk = require('chalk');
 const boxen = require('boxen');
 const yargs = require('yargs');
-const fetch = require('node-fetch');
-const fs = require('fs');
 const version = require('./package.json').version;
 const { boxenOptions } = require('./styling/style');
+const fileHandler = require('./lib/fileProcessing');
 
 const vmessage = chalk.white.bold(`Utest version: ${version}`);
 const msgBox = boxen(vmessage, boxenOptions);
@@ -26,63 +24,13 @@ const argv = yargs
   .version(`${msgBox}`)
   .describe('version', 'show version information').argv;
 
-// Create stream with the file
-const s = fs.createReadStream(argv.file);
-
-let urlList;
-s.on('data', (buf) => {
-  // Get all the URL links from the file
-  urlList = buf.toString().match(/(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]+)/gi);
-});
-
-//print responses colorized
-function printResponse(data) {
-  for (var item of data) {
-    if (item.status == 200) {
-      console.log(chalk.green.bold(`[GOOD] Status: [${item.status}] ${item.url}`));
-    } else if (item.status == 400 || item.status == 404) {
-      console.log(chalk.red.bold(`[BAD] Status: [${item.status}] ${item.url}`));
-    } else {
-      console.log(chalk.grey.bold(`[UNKNOWN] Status: [${item.status}] ${item.url}`));
-    }
-  }
-}
-
-function checkStatus(data) {
-  for (var item of data) {
-    if (item.status == 400 || item.status == 404 || item.status == 'UNKNOWN') {
-      return 1;
-    }
-  }
-  return 0;
-}
-
-s.on('end', async () => {
-  var responseStatusByUrl = [];
-  var statusResponseForUrl;
-
-  //Iterate through the links and check their status
-
-  await Promise.all(
-    urlList.map(async (url) => {
-      try {
-        const urlTest = await fetch(url, { method: 'head', timeout: 1500 });
-        statusResponseForUrl = { url: `${url}`, status: `${urlTest.status}` };
-        responseStatusByUrl.push(statusResponseForUrl);
-      } catch (error) {
-        statusResponseForUrl = { url: `${url}`, status: 'UNKNOWN' };
-        responseStatusByUrl.push(statusResponseForUrl);
-      }
-    })
-  );
+if (argv.file) {
   if (argv.json) {
-    console.log(JSON.stringify(responseStatusByUrl));
+    fileHandler.processFile(argv.file, true);
   } else {
-    printResponse(responseStatusByUrl);
+    fileHandler.processFile(argv.file);
   }
-
-  process.exit(checkStatus(responseStatusByUrl));
-});
+}
 process.on('exit', function (code) {
   return console.log(`About to exit with code ${code}`);
 });
